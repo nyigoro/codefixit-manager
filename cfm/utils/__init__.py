d9c8bbfe8fac642192491db3d464eb5d3a6ee3d3
nyigoro <nyigoro@gmail.com>
2025-07-02 14:13:31 +0300

    Update __init__.py

--- cfm/utils/__init__.py
+++ cfm/utils/__init__.py
@@ -1,3 +1,5 @@
-touch cfm/__init__.py
-touch cfm/engine/__init__.py
-touch cfm/utils/__init__.py
+# cfm/__init__.py
+
+from importlib.metadata import version
+
+__version__ = version("codefixit-manager")
