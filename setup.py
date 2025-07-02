77f3e5c66bb2e7a7f9e2d23fbe14fd7e062022e1
nyigoro <nyigoro@gmail.com>
2025-07-02 13:39:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="codefixit-manager",
-    version="1.0.2",
+    version="1.0.3",
     description="Modernize and auto-refactor legacy code using rule-based fixers.",
     author="Nyigoro",
     author_email="nyigoro@gmail.com",
